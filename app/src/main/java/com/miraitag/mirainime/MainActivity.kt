7fad966ac80d22b75b32f3571f0628f60171d8bd
miraitag <ruiztahuilan@gmail.com>
2026-04-05 21:13:52 -0600

    Feat(App): Init new app mirainime

---
+++ app/src/main/java/com/miraitag/mirainime/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.miraitag.mirainime
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import com.miraitag.mirainime.ui.theme.MirainimeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MirainimeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Text(text = "Hello World", modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
